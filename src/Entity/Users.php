a2b3e286375555be141e095835d63d2e4bfca1a9
Free5Dev <saidsoumah@gmail.com>
2019-03-12 22:35:50 +0100

    projet bitchest app de vente de d'achat de crypto monnaie

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -2,11 +2,19 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ *  * @UniqueEntity(
+ *  fields={"email"},
+ *  message="Un autre utilisateur s'est deja inscrit avec cette adresse mail, merci de la modifier "
+ * )
  */
 class Users implements UserInterface
 {
@@ -19,13 +27,33 @@ class Users implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     *@Assert\NotBlank(message="veillez , renseignez votre nom")
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="veillez , renseignez votre prenom")
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="veillez , renseignez votre username")
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=8, minMessage="Le mot de passe dois faire minum 8 caractères")
      */
     private $password;
+    /**
+     * Faire une constrainte d'egalité
+     *@Assert\EqualTo(propertyPath="password", message="Saisier le même mots de passe")
+     * @var string
+     */
+    public $confirm_password;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -35,46 +63,115 @@ class Users implements UserInterface
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $firstname;
+    private $ville;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $lastname;
+    private $adresse;
 
     /**
      * @ORM\Column(type="string", length=255)
-     */
-    private $city;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="integer")
      */
     private $cp;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="string", length=255)
      */
-    private $phone;
+    private $telephone;
 
     /**
      * @ORM\Column(type="float")
      */
     private $balance;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $picture;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Transaction", mappedBy="users")
+     */
+    private $transactions;
+
+    /**
+     * Function qui return le nolm au complet
+     *
+     */
+    
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Wallet", mappedBy="users")
+     */
+    private $wallets;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Role", mappedBy="users")
+     */
+    private $userRoles;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Buy", mappedBy="users")
+     */
+    private $buys;
+
+    
+
+   
+    /**
+     * Function qui return le nolm au complet
+     *
+     */
+    public function getFullName(){
+        return "{$this->nom} {$this->prenom} ";
+    }
+
+    public function __construct()
+    {
+        $this->transactions = new ArrayCollection();
+        $this->wallets = new ArrayCollection();
+        $this->userRoles = new ArrayCollection();
+        $this->achats = new ArrayCollection();
+        $this->buys = new ArrayCollection();
+    }
+
+    
+
+
+    
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
     public function getUsername(): ?string
     {
-        return $this->email;
+        return $this->username;
     }
 
     public function setUsername(string $username): self
@@ -108,74 +205,50 @@ class Users implements UserInterface
         return $this;
     }
 
-    public function getFirstname(): ?string
+    public function getVille(): ?string
     {
-        return $this->firstname;
+        return $this->ville;
     }
 
-    public function setFirstname(string $firstname): self
+    public function setVille(string $ville): self
     {
-        $this->firstname = $firstname;
+        $this->ville = $ville;
 
         return $this;
     }
 
-    public function getLastname(): ?string
+    public function getAdresse(): ?string
     {
-        return $this->lastname;
+        return $this->adresse;
     }
 
-    public function setLastname(string $lastname): self
+    public function setAdresse(string $adresse): self
     {
-        $this->lastname = $lastname;
+        $this->adresse = $adresse;
 
         return $this;
     }
 
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getCp(): ?int
+    public function getCp(): ?string
     {
         return $this->cp;
     }
 
-    public function setCp(int $cp): self
+    public function setCp(string $cp): self
     {
         $this->cp = $cp;
 
         return $this;
     }
 
-    public function getPhone(): ?int
+    public function getTelephone(): ?string
     {
-        return $this->phone;
+        return $this->telephone;
     }
 
-    public function setPhone(int $phone): self
+    public function setTelephone(string $telephone): self
     {
-        $this->phone = $phone;
+        $this->telephone = $telephone;
 
         return $this;
     }
@@ -192,8 +265,161 @@ class Users implements UserInterface
         return $this;
     }
     public function getRoles(){
-        return ['ROLES_USER'];
+        
+        $roles=$this->userRoles->map(function($role){
+            return $role->getTitle();
+        })->toArray();
+        $roles[]='ROLE_USER';
+        
+        return $roles;
+        $roles=$this->userRoles->map(function($role){
+            return $role->getTitle();
+        })->toArray();
+        $roles[]='ROLE_USER';
+        
+        return $roles;
     }
-    public function getSalt(){}
-    public function eraseCredentials(){}
+    public function getSalt(){
+
+    }
+    public function eraseCredentials(){
+        
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Transaction[]
+     */
+    public function getTransactions(): Collection
+    {
+        return $this->transactions;
+    }
+
+    public function addTransaction(Transaction $transaction): self
+    {
+        if (!$this->transactions->contains($transaction)) {
+            $this->transactions[] = $transaction;
+            $transaction->setUsers($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTransaction(Transaction $transaction): self
+    {
+        if ($this->transactions->contains($transaction)) {
+            $this->transactions->removeElement($transaction);
+            // set the owning side to null (unless already changed)
+            if ($transaction->getUsers() === $this) {
+                $transaction->setUsers(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Wallet[]
+     */
+    public function getWallets(): Collection
+    {
+        return $this->wallets;
+    }
+
+    public function addWallet(Wallet $wallet): self
+    {
+        if (!$this->wallets->contains($wallet)) {
+            $this->wallets[] = $wallet;
+            $wallet->setUsers($this);
+        }
+
+        return $this;
+    }
+
+    public function removeWallet(Wallet $wallet): self
+    {
+        if ($this->wallets->contains($wallet)) {
+            $this->wallets->removeElement($wallet);
+            // set the owning side to null (unless already changed)
+            if ($wallet->getUsers() === $this) {
+                $wallet->setUsers(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Role[]
+     */
+    public function getUserRoles(): Collection
+    {
+        return $this->userRoles;
+    }
+
+    public function addUserRole(Role $userRole): self
+    {
+        if (!$this->userRoles->contains($userRole)) {
+            $this->userRoles[] = $userRole;
+            $userRole->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserRole(Role $userRole): self
+    {
+        if ($this->userRoles->contains($userRole)) {
+            $this->userRoles->removeElement($userRole);
+            $userRole->removeUser($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Buy[]
+     */
+    public function getBuys(): Collection
+    {
+        return $this->buys;
+    }
+
+    public function addBuy(Buy $buy): self
+    {
+        if (!$this->buys->contains($buy)) {
+            $this->buys[] = $buy;
+            $buy->setUsers($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBuy(Buy $buy): self
+    {
+        if ($this->buys->contains($buy)) {
+            $this->buys->removeElement($buy);
+            // set the owning side to null (unless already changed)
+            if ($buy->getUsers() === $this) {
+                $buy->setUsers(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+    
+    
 }
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -3,26 +3,36 @@
 namespace App\Form;
 
 use App\Entity\Users;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
-class RegistrationType extends AbstractType
+
+
+class RegistrationType extends ApplicationType
 {
+   
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password',PasswordType::class)
-            ->add('email')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('city')
-            ->add('address')
-            ->add('cp')
-            ->add('phone')
-            ->add('balance')
+            ->add('nom', TextType::class, $this->getConfiguration("Nom", "Entrer votre nom..."))
+            ->add('prenom',TextType::class, $this->getConfiguration("Prenom", "Entrer votre Prenom..."))
+            ->add('username',TextType::class, $this->getConfiguration("Nom d'utilisateur","Entrer votre nom d'utilisateur"))
+            ->add('password',PasswordType::class,$this->getConfiguration("Mot de passe", "Entrer votre mot de passe"))
+            ->add('confirm_password',PasswordType::class,$this->getConfiguration("Confirmer Mot de passe", "Répéter le mot de passe"))
+
+            ->add('email',TextType::class, $this->getConfiguration("Email", "Entrer votre Email"))
+            ->add('ville',TextType::class, $this->getConfiguration("Ville", "Tapez votre ville"))
+            ->add('adresse',TextType::class, $this->getConfiguration("Adresse", "Entrer votre adresse"))
+            ->add('cp',TextType::class, $this->getConfiguration("Code postal", "Entrer votre code postal"))
+            ->add('telephone',TextType::class, $this->getConfiguration("Télephone", "Votre numero de téléphone"))
+            ->add('balance',IntegerType::class, $this->getConfiguration("Solde", "Entrer votre solde"))
+            ->add('picture',UrlType::class, $this->getConfiguration("Url de la photo","Entrer l'url de votre avatar"))
         ;
     }
 
